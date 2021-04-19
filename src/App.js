03fdc84471a49fe7ef0ddebdc096be153bb3096b
Bombel <bombel@bombel.com>
2021-04-19 19:56:05 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import Sun from "./Icon/Sun";
+import {useState} from "react";
+import Loupe from "../src/Icon/Loupe";
+import Cloudy from "./Icon/Cloudy";
+
+const api = {
+    key: "55e7b3a3ccbe4572d148829af1556fc5",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day}, ${date} ${month} ${year}`;
+    }
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+
+    const fetchDataAboutWeather = () => {
+        const url = `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`;
+        console.log(url);
+
+        fetch(url)
+            .then(res => res.json())
+            .then(result => {
+                    setWeather(result);
+                    setQuery('');
+                    console.log(result);
+                }
+            );
+    }
+
+    const getWeather = () => {
+        fetchDataAboutWeather();
+    }
+
+    const search = e => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            fetchDataAboutWeather();
+        }
+    }
+
+    return (
+        <div className="app">
+            <div className="containerSearch">
+                <h1>Weather App</h1>
+                <div className="form">
+                    <div>
+                        <Loupe />
+                    </div>
+                    <input
+                        type="text"
+                        placeholder=""
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    ></input>
+                    <div className="line"></div>
+                    <button
+                        type="submit"
+                        onClick={() => getWeather()}
+                    >SEARCH</button>
+                </div>
+            </div>
+            {(typeof weather.main != "undefined")
+            ?
+                <div className="containerDisplay">
+                    <p className="titleDisplay">{weather.name}, {weather.sys.country}</p>
+                    <div className="textDisplay">{dateBuilder(new Date())}</div>
+                    <div className="textDisplay type">{weather.weather[0].main}</div>
+                    <div className="iconDisplay">
+                        {weather.weather[0].main === "Clouds"
+                        ?
+                            <div>
+                                <Cloudy />
+                            </div>
+                        :
+                            <div>
+                                <Sun />
+                            </div>
+                        }
+
+                    </div>
+                    <p className="temperatureDisplay">{Math.round(weather.main.temp)}°C</p>
+                </div>
+            :
+                ('')
+            }
+        </div>
+    );
 }
 
 export default App;
